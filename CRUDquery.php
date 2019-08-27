c08740b0213820a0674b368f3506663e8762a0b2
Ravindu Kariyapperuma <ravindu.kariyapperuma@gmail.com>
2019-08-27 11:51:43 +0530

    insert query finished

--- CRUDquery.php
+++ CRUDquery.php
@@ -10,6 +10,16 @@ if(isset($_POST['save'])) {
 		
 		$iQuery = "INSERT INTO account(username, password) values(?,?)";
 		$stmt = $conn->prepare($iQuery);
+		$stmt->bind_param("ss", $username, $password);
+		if($stmt->execute()){
+			#alert msg
+		}
+		$stmt->close();
+		$conn->close();
 	}
+	else{
+		#alert msg
+	}
+	header("location: index.php")
 }
 ?>
